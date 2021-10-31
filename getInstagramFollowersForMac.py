c2ea3dae565ab25d473b0e5af870226d077d8a71
mima-k <mimakouhei@gmail.com>
2021-10-31 18:42:50 +0900

    driverに変更

--- getInstagramFollowersForMac.py
+++ getInstagramFollowersForMac.py
@@ -14,18 +14,18 @@ def main():
 
     username = settings.USERNAME
     print('target account: ' + username)
-    # driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver')
+    driver = webdriver.Chrome()
 
     # ログインページに遷移する
-    browser.get('https://www.instagram.com/accounts/login/?source=auth_switcher')
+    driver.get('https://www.instagram.com/accounts/login/?source=auth_switcher')
 
     # 2秒スリープ（待機）
     time.sleep(2)
 
     # ユーザ名、パスワードを入力してログイン
-    id = browser.find_element_by_name("username")
+    id = driver.find_element_by_name("username")
     id.send_keys(username)
-    password = browser.find_element_by_name("password")
+    password = driver.find_element_by_name("password")
     password.send_keys(settings.PASSWORD)
     password.send_keys(Keys.RETURN)
 
@@ -33,13 +33,13 @@ def main():
     time.sleep(3)
 
     # 対象アカウントのInstagramページにアクセス
-    browser.get('https://www.instagram.com/' + username + '/')
+    driver.get('https://www.instagram.com/' + username + '/')
 
     # 3秒スリープ（待機）
     time.sleep(3)
 
     # 画面上で、フォロワーのリンクをクリック
-    follower_button = browser.find_elements_by_css_selector("li.Y8-fY")[1]
+    follower_button = driver.find_elements_by_css_selector("li.Y8-fY")[1]
     follower_button.click()
 
     # 3秒スリープ（待機）
@@ -47,12 +47,12 @@ def main():
 
 
     # フォロワーの一覧は、ポップアップウインドウで表示されます
-    dialog = browser.find_element_by_css_selector("div.isgrP")
+    dialog = driver.find_element_by_css_selector("div.isgrP")
     for i in range(200):
-        browser.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", dialog)
+        driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", dialog)
         time.sleep(random.randint(500,1000)/1000)
 
-    page_url = browser.page_source
+    page_url = driver.page_source
     soup = BeautifulSoup(page_url,"lxml")
     elements = soup.find_all("a", {"class": "FPmhX notranslate _0imsa"})
     followers = []
@@ -67,7 +67,7 @@ def main():
     now = datetime.datetime.now()
     df.to_csv('csv/' + username + '_follower_list_' + now.strftime('%Y%m%d_%H%M%S') + '.csv')
 
-    browser.close()
+    driver.close()
 
 if __name__ == '__main__':
     main()
